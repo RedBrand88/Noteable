cf12252a307faa9b3fabf53119edc71584a446d4
Brandon Bashein <brandon@thefoodtruckleague.com>
2020-02-23 16:11:03 -0700

    first commit

---
+++ noteable.py
@@ -0,0 +1,49 @@
+import sys
+import os
+from textfilecreator import textfilecreator
+
+python = ".py"
+js = ".js"
+jsx = ".jsx"
+text = ".txt"
+
+extensions = {
+    "py": python,
+    ".py": python,
+    "python": python,
+    "js": js,
+    ".js": js,
+    "javascript": js,
+    "jsx": jsx,
+    ".jsx": jsx,
+    "react": jsx,
+    "text": text,
+    ".txt": text,
+    "txt": text,
+}
+
+
+def create():
+    parentdirectory = "~/Documents/Notes/"
+    extension = str(sys.argv[3])
+
+    try:
+        extension = extensions[extension]
+    except Exception as e:
+        print("failed to create file type " + str(e))
+        extension = ".txt"
+
+    filename = str(sys.argv[1]) + extension
+    foldername = str(sys.argv[2])
+
+
+    with textfilecreator(filename, foldername, parentdirectory):
+        pass
+
+    filepath = os.path.join(parentdirectory, foldername, filename)
+
+    os.system("vim " + filepath)
+
+if __name__ == "__main__":
+    create()
+
---
+++ textfilecreator.py
@@ -0,0 +1,27 @@
+import os
+
+class textfilecreator:
+    # object created to use with statement
+    # this class creates a file in ~/Documents if it doesn't already exist
+
+    # this is a copy of ~Dev/punchcard/textfilecreator.py until I figure out how to make a module
+
+    def __init__(self, filename, directory, parentdirectory="~/Documents"):
+        self.filename = filename
+        self.directory = directory
+        self.parentdirectory = parentdirectory
+        self.path = os.path.expanduser(os.path.join(self.parentdirectory, self.directory))
+
+    def __enter__(self):
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+            completepath = os.path.join(self.path, self.filename)
+            self.file = open(completepath, 'a+')
+            return self.file
+        else:
+            completepath = os.path.join(self.path, self.filename)
+            self.file = open(completepath, 'a+')
+            return self.file
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.file.close()
